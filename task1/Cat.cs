0ad6d3b550939bf4543ecaeca82689e999f86041
Pavel Bobylev <paha_ska_15@mail.ru>
2022-04-20 10:31:28 +0300

    Добавьте файлы проекта.

---
+++ task1/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Cat : Animal
+    {
+        string name;
+        public override string Name 
+        { 
+            get => name; 
+            set => name = value; 
+        }
+        public Cat(string name)
+            :base (name)
+        {
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяф");
+        }
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Гав");
+            Dog dog = new Dog("Кузя");
+
+            cat.ShowInfo();
+            dog.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
